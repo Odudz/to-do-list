0983eb3bff2f9c743ed6ab46ec441111e9aa5a42
Dudz <142178060+Odudz@users.noreply.github.com>
2025-10-06 11:36:06 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,41 @@
+class Task{
+    constructor(description){
+        this.description = description;
+        this.completed = false;
+    }
+
+    toggleComplete(){
+        this.completed = this.completed;
+    }
+}
+class TaskManager{
+    constructor(){
+        this.task = [];
+    }
+
+    addTask(description){
+        const task = new Task(description);
+        this.tasks.push(task);
+        this.displayTask();
+    }
+
+    removeTask(index){
+        this.tasks.splice(index, 1);
+        this.displayTask();
+    }
+
+    toggleTaskCompletition(index){
+        this.tasks[index].toggleComplete();
+        this.displayTask();
+    }
+
+    displayTask(){
+        const taskList = document.getElementById('task-list');
+        taskList.innerHTML = '';
+
+        this.tasks.forEach((task, index) =>{
+            const taskItem = document.createElement('li');
+            taskItem.className = task.completed ? 'completed' : '';
+        })
+    }
+}
