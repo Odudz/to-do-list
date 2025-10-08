2fc2d951e51ce2a0aeafc8767a16035a8162e2f7
Dudz <142178060+Odudz@users.noreply.github.com>
2025-10-08 08:47:39 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,64 +1,55 @@
-class Task{
-    constructor(description){
-        this.description = description;
-        this.completed = false;
-    }
+import sqlite3 from "sqlite3";
+import { open } from 'sqlite';
+import express from "express";
 
-    toggleComplete(){
-        this.completed = this.completed;
-    }
-}
-class TaskManager{
-    constructor(){
-        this.task = [];
-    }
+const app = express();
+const PORT = 3000;
 
-    addTask(description){
-        const task = new Task(description);
-        this.tasks.push(task);
-        this.displayTask();
-    }
+app.use(express.json());
+app.use(express.static('public/'))
 
-    removeTask(index){
-        this.tasks.splice(index, 1);
-        this.displayTask();
-    }
+let db;
+async function initDB(){
+    db = await open({
+        filename: './banco.db',
+        driver:sqlite3.Database,
+    });
 
-    toggleTaskCompletition(index){
-        this.tasks[index].toggleComplete();
-        this.displayTask();
-    }
-
-    displayTask(){
-        const taskList = document.getElementById('task-list');
-        taskList.innerHTML = '';
-
-        this.tasks.forEach((task, index) =>{
-            const taskItem = document.createElement('li');
-            taskItem.className = task.completed ? 'completed' : '';
-
-            const taskDescription = document.createElement('span');
-            taskDescription.textContent = taskDescription;
-            taskDescription.addEventListener('click', () => this.toggleTaskCompletition(index));
-
-            const removeButton = document.createElement('button');
-            removeButton.className = 'remove-btn';
-            removeButton.textContent = 'Remove';
-            removeButton.addEventListener('click', () => this.removeTask(index));
-
-            taskItem.appendChild(taskDescription);
-            taskItem.appendChild(removeButton);
-            taskItem.appendChild(taskItem);
-        })
-    }
+    await db.run(`REATE TABLE IF NOT EXISTS tasks(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        description TEXT NOT null,
+        completed INTEGER DEFAULT 0
+    )`);
 }
 
-document.addEventListener('DOMContentLoaded', () =>{
-    const TaskManager = new TaskManager();
-    const addTaskBtn = document.getElementById('add-task-btn');
-    const taskInput = document.getElementById('task-input');
+app.get('./tasks', async(req,res) => {
+    const task = await db.all(`SELECT * FROM tasks`);
+    res.json(tasks);
+});
 
-    addTaskBtn.addEventListener('click', () =>{
-        const taskDescription = taskInput.value.trim();
-    })
+app.post('/tasks', async(req, res) => {
+    const { description } = req.body;
+    const stmt = await db.prepare(`INSERT INTO tasks (description) VALUES (?)`);
+    await stmt.run(description);
+    await stmt.finalize();
+    res.status(201).json({message: 'Task added'});
 })
+
+app.post('/tasks/:id', async(req,res) =>{
+    const { id } = req.params;
+    await db.run(`DELETE FROM tasks WHERE id = ?`, id);
+    res.status(204).send();
+});
+
+app.patch('tasks/:id/toggle', async(req, res) => {
+    const { id } = req.params;
+    const task = await db.get(`SELECT * FROM tasks WHERE id = ?`, id);
+    const completed = task.completed ? 0 : 1;
+    await db.run(`UPTADED tasks SET completed = ? WHERE id = ?`, completed, id);
+    res.status(204).send();
+})
+
+app.listen(PORT, async() => {
+    await initDB();
+    console.log(`server running at http://localhost:${PORT}`);
+});
